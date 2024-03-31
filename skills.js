5f314b5d4d7b8ee512ca98995e432442dd2f388a
DarienOOM <102061697+DarienOOM@users.noreply.github.com>
2024-03-31 21:26:14 +0000

    Copilot third commit

--- skills.js
+++ skills.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 function calculateNumbers(var1, var2) {
     return var1 + var2;
 }   
+=======
+function calculatenumbers(var1, var2) {
+  return var1 + var2;
+}
+>>>>>>> d71049f (Copilot third commit)
